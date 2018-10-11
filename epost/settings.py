2708ad6ae87f8cc41047ae9dc343254c896ff785
Cifer <cifer1027@gmail.com>
2018-10-11 16:33:03 +1100

    notice model

--- epost/settings.py
+++ epost/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'epost',
         'USER':'root',
-        'PASSWORD':'fy8824392',
+        'PASSWORD':'root',
         'HOST':'127.0.0.1',
         'PORT':'3306',
     }
