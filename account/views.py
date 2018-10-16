94dac3e1513dcd848fa1d993720bd66c9edce224
Cifer <cifer1027@gmail.com>
2018-10-16 17:10:22 +1100

    login and register

--- account/views.py
+++ account/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from . import models
 from . import forms
+from face_recognition
 
 
 import hashlib
@@ -75,7 +76,6 @@ def register(request):
                 if same_email_user:
                     message = 'The email address has already been registered. Please use another email address！'
                     return render(request, 'login/register.html', locals())
-            return render(request, 'login/confirm.html', locals())
     register_form = forms.RegisterForm()
     return render(request, 'login/register.html', locals())
 
@@ -86,7 +86,76 @@ def logout(request):
     request.session.flush()
     return redirect('/index/')
 
+def loginFaceCheck(request):
 
+    ## 人脸登陆验证
+
+    if request.method == "POST" and request.is_ajax():
+        # 获取base64格式的图片
+        faceImage = request.POST.get('faceImg')
+        # 提取出base64格式，并进行转换为图片
+        index = faceImage.find('base64,')
+        base64Str = faceImage[index+6:]
+        img = base64.b64decode(base64Str)
+        # 将文件保存
+        backupDate = time.strftime("%Y%m%d_%H%M%S")
+        if int(request.POST.get('id')) == 0 :
+            fileName = BASE_LOGIN_LEFT_PATH +"LeftImg_%s.jpg" % (backupDate)
+        else:
+            fileName = BASE_LOGIN_RIGHT_PATH + "RightImg_%s.jpg" % (backupDate)
+        file = open(fileName, 'wb')
+        file.write(img)
+        file.close()
+        # 删除多余的图片
+        filesLeft = os.listdir(BASE_LOGIN_LEFT_PATH)
+        filesLeft.sort()
+        leftImgCount = filesLeft.__len__()
+        filesRight = os.listdir(BASE_LOGIN_RIGHT_PATH)
+        filesRight.sort()
+        RightImgCount = filesRight.__len__()
+
+        if leftImgCount > 100:
+            # 图片超过100个，删除一个
+            os.unlink(BASE_LOGIN_LEFT_PATH +filesLeft[0])
+        if RightImgCount > 100:
+            # 图片超过100个，删除一个
+            os.unlink(BASE_LOGIN_RIGHT_PATH + filesRight[0])
+
+        # 对图片进行人脸识别比对
+        canLogin = False
+        AuthName = "未授权用户"
+
+        # 1> 加载相机刚拍摄的人脸
+        unknown_face = face_recognition.load_image_file(fileName)
+        unknown_face_tmp_encoding = []
+        try:
+            unknown_face_tmp_encoding = face_recognition.face_encodings(unknown_face)[0]
+        except IndexError:
+            canLogin = False  # 图片中未发现人脸
+
+        # 2> 进行比对
+
+        ### 第一种方法
+        # results = face_recognition.face_distance(known_face,unknown_face_tmp_encoding)
+        # 小于0.6即对比成功。但是效果不好，因此我们设置阈值为0.4,
+        # for i, face_distance in enumerate(results):
+        #     if face_distance <= 0.4:
+        #         canLogin = True
+        #         AuthName = os.listdir(BASE_LOGIN_AUTH_PATH)[i][:-4]
+
+        ### 第二中方法
+        results1 = face_recognition.compare_faces(known_face,unknown_face_tmp_encoding,0.4)
+        for i, face_distance in enumerate(results1):
+            if face_distance == True:
+                canLogin = True
+                AuthName = os.listdir(BASE_LOGIN_AUTH_PATH)[i][:-4]
+
+        JsonBackInfo = {
+            "canLogin": canLogin,
+            "AuthName": AuthName
+        }
+
+        return JsonResponse(JsonBackInfo)
 
 
 
